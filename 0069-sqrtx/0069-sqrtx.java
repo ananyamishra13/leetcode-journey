f8a17fc11b2543df8a362972a6df000ca0799db2
Ananya Mishra <a.ananya0308@gmail.com>
2026-05-16 12:07:40 +0530

    Time: 1 ms (97.94%), Space: 42.3 MB (94.42%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,12 +1,12 @@
 class Solution {
-    public static int mySqrt(int x) {
+    public  int mySqrt(int x) {
      double sqrt_x = Math.pow(x,0.5);       
      
               return (int)sqrt_x;
 
     }
 
-    public static void main(String args[]){
+    public  void main(String args[]){
         System.out.println("square root of no is " + mySqrt(4));
     }
 
